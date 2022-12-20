a0f6b14de59d5586917c5566ba1be569b1d2be38
jake <th3hopperr@gmail.com>
2022-12-20 13:26:57 -0500

    first stage

---
+++ db/index.js
@@ -0,0 +1,45 @@
+// inside db/index.js
+const { Client } = require('pg'); // imports the pg module
+
+// supply the db name and location of the database
+const client = new Client({
+  user: 'jake',
+  host: 'localhost',
+  database: 'juicebox-dev',
+  password: '1234',
+  port: 5432,
+}
+);
+
+
+
+async function createUser({ username, password }) {
+  try {
+    const result = await client.query(`
+      INSERT INTO users(username, password) 
+      VALUES($1, $2) 
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
+    `, [username, password]);
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllUsers() {
+  const { rows } = await client.query(
+    `SELECT id, username 
+    FROM users;
+  `);
+
+  return rows;
+}
+
+
+module.exports = {
+  client,
+  getAllUsers,
+  createUser
+}
